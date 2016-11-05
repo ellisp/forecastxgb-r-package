0bb0f94bc3749484db7ee55d4fa6c99cc02baff6
Peter Ellis <peter.ellis2013nz@gmail.com>
2016-11-06 10:08:29 +1300

    improve documentation

--- pkg/R/extras.R
+++ pkg/R/extras.R
@@ -42,3 +42,51 @@ print.summary.xgbts <- function(x, ...){
   cat(paste0(x$n, " original observations.\n", 
             x$effectn, " effective observations after creating lagged features.\n"))
 }
+
+
+#' Plot xgbts object
+#' 
+#' plot method for an object created by xgbts
+#' @export
+#' @import graphics
+#' @method plot xgbts
+#' @param x An object created by \code{xgbts}
+#' @param ... Additional arguments passed through to \code{plot()}
+plot.xgbts <- function(x, ...){
+  ts.plot(x$y, col = "brown", ...)
+  lines(x$fitted, col = "blue")
+  
+}
+
+
+#' Tourism forecasting results
+#' 
+#' Summary data from four models, and 11 combinations of models, against the data from the 2010 tourism forecasting competition.
+#' 
+#' Full details of how this was generated are in the Vignette.  This shows the average mean absolute scaled error 
+#' (MASE) from using `xgbts` (x), `auto.arima` (a), `nnetar` (n) and `thetaf` (f) to generate forecasts of 1,311 tourism data series.
+#' 
+#' 
+#' \itemize{
+#' \item MASE A mean mean absolute squared error
+#' \item model model, or ensemble of models, to which the MASE applies
+#' \item Frequency The frequency of the subset of data from which the mean MASE was calculated.
+#' }
+#' @format A data frame with 60 rows and three columns.
+#' @examples
+#' if(require(ggplot2)){
+#' leg <- "f: Theta; forecast::thetaf\na: ARIMA; forecast::auto.arima
+#' n: Neural network; forecast::nnetar\nx: Extreme gradient boosting; forecastxgb::xgbts"
+#' 
+#' ggplot(Tcomp_results, aes(x = model, y =  MASE, colour = Frequency, label = model)) +
+#'   geom_text(size = 4) +
+#'   geom_line(aes(x = as.numeric(model)), alpha = 0.25) +
+#'   annotate("text", x = 2, y = 3.5, label = leg, hjust = 0) +
+#'   ggtitle("Average error of four different timeseries forecasting methods
+#'2010 Tourism Forecasting Competition data") +
+#'   labs(x = "Model, or ensemble of models
+#'(further to the left means better overall performance)",
+#'   y = "Mean scaled absolute error\n(smaller numbers are better)") +
+#'   theme_grey(9)
+#'   }
+"Tcomp_results"
